b05ae0ee1b121170b3423671a8f7f82c69e88966
edit <ghislaincamille8@gmail.com>
2026-02-18 12:47:40 -0800

    Fix CORS: allow x-admin-password header in preflight

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key', 'x-admin-password']
 }));
 
 app.use(express.json());
