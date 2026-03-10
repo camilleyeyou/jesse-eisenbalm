9cbfdb1d274be56bb773fca807bb50b82e69d193
edit <ghislaincamille8@gmail.com>
2026-03-10 05:43:01 -0700

    Add Google Analytics 4 (GA4) tracking with Core Web Vitals reporting

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,26 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
-      onCLS(onPerfEntry);
-      onINP(onPerfEntry);
-      onFCP(onPerfEntry);
-      onLCP(onPerfEntry);
-      onTTFB(onPerfEntry);
+function sendToGA4({ name, delta, id }) {
+  if (typeof window.gtag === 'function') {
+    window.gtag('event', name, {
+      event_category: 'Web Vitals',
+      value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+      event_label: id,
+      non_interaction: true,
     });
   }
+}
+
+const reportWebVitals = onPerfEntry => {
+  const callback = onPerfEntry && onPerfEntry instanceof Function
+    ? (metric) => { sendToGA4(metric); onPerfEntry(metric); }
+    : sendToGA4;
+
+  import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+    onCLS(callback);
+    onINP(callback);
+    onFCP(callback);
+    onLCP(callback);
+    onTTFB(callback);
+  });
 };
 
 export default reportWebVitals;
